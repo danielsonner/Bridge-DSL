073b60611b367fb666ed61c0eda0bf3f30667270
Daniel <daniel.sonner@gmail.com>
2016-11-13 10:23:22 -0800

    hand may not be inputted nicer

--- internal/src/Internal.scala
+++ internal/src/Internal.scala
@@ -4,7 +4,7 @@ case class Auction(a : String)
   def display(afterHowLong : Int = 0) = {println(s"N  E  S  W\n$a")}
 }
 
-case class Card(value : Char, suit: Char) // we will use T for 10 for now
+case class Card(value : Char, suit: Char) // we T for 10 for now
 {
   override def toString: String = (value.toString)
 }
@@ -13,18 +13,18 @@ case class Hand(h : List[Card])
   override def toString: String = 
   {
     val SPACER = "             "
-    val nSpades = h.filter(_.suit == 's').mkString("")
-    val nHearts = h.filter(_.suit == 'h').mkString("")
-    val nClubs = h.filter(_.suit == 'c').mkString("")
-    val nDiamonds = h.filter(_.suit == 'd').mkString("")
+    val nSpades = "♠" + h.filter(_.suit == 's').mkString("")
+    val nHearts = "♥" + h.filter(_.suit == 'h').mkString("")
+    val nClubs = "♣" + h.filter(_.suit == 'c').mkString("")
+    val nDiamonds = "♦" + h.filter(_.suit == 'd').mkString("")
     s"$SPACER$nSpades\n$SPACER$nHearts\n$SPACER$nDiamonds\n$SPACER$nClubs"
   }
   def listStringHands: List[String] = 
   {
-    val nSpades = h.filter(_.suit == 's').mkString("")
-    val nHearts = h.filter(_.suit == 'h').mkString("")
-    val nDiamonds = h.filter(_.suit == 'd').mkString("")
-    val nClubs = h.filter(_.suit == 'c').mkString("")
+    val nSpades = "♠" + h.filter(_.suit == 's').mkString("")
+    val nHearts = "♥" + h.filter(_.suit == 'h').mkString("")
+    val nDiamonds = "♣" + h.filter(_.suit == 'd').mkString("")
+    val nClubs = "♦" + h.filter(_.suit == 'c').mkString("")
     List(nSpades,nHearts,nDiamonds,nClubs)
   }
 }
@@ -53,6 +53,17 @@ object BridgeExtender {
 			Card(rank, suit)
 		}
 	
+	implicit def StringToCardL(value : String) : Hand =
+	{
+	  val sepSuits = (value.map{ x => if (x == '1') 'T' else x}.filter(_ != '0') // change 10 to T
+	                  .split(Array('s', 'h', 'd', 'c')).map(_.trim)) // split by suit
+	  val cardL : List[Card] = (sepSuits(0).map{ x => Card(x,'s')}.toList
+	                           ::: sepSuits(1).map{ x => Card(x,'h')}.toList
+	                           ::: sepSuits(2).map{ x => Card(x,'d')}.toList
+	                           ::: sepSuits(3).map{ x => Card(x,'c')}.toList)
+	  Hand(cardL)
+	}
+
 	implicit def StringToAuction(value : String) : Auction =
 	{
 	  Auction(value)
--- internal/src/Program.scala
+++ internal/src/Program.scala
@@ -5,24 +5,26 @@ object Program extends App{
   val auction : Auction = "-  3d 3N AP" // maybe specify dealer and skip -
   
   
-  val N : Hand = Hand(List(  // mildly annoying to have this much typing to write
-      "10s", "6s", "5s", "3s",
-      "Kh", "Qh", "5h", "4h",
-      "7d",
-      "Kc","Jc","8c","5c"
-      ))
-  val E : Hand = Hand(List(
-      "-s", //TODO: Handle this in Internal.scala and let user not write this line at all
-      "10h", "3h",
-      "Kd","Jd","10d","6d","4d","3d","2d",
-      "Qc","9c","4c","3c"
-      ))
-  val S : Hand = Hand(List(
-      "As", "Ks", "9s", "4s",
-      "8h", "6h",
-      "Ad", "Qd", "4d",
-      "Ac","7c","6c","2c"
-      ))
+  val N : Hand =  """
+                  10653s
+                  KQ54h
+                  7d
+                  KJ85c
+                  """ // string could've been one line but I think multiline style nicer
+
+  val E : Hand = 
+      """
+      - s
+      103h
+      KJ106432d
+      Q943c
+      """//TODO: Handle voids in Internal.scala and let user not write voided suits
+  val S : Hand = """
+      AK94s
+      86h
+      AQ4d
+      A762c
+      """
   val W : Hand = Hand(List(
       "Qs", "Js", "8s", "7s", "2s",
       "Ah", "Jh", "9h", "7h", "2h",
